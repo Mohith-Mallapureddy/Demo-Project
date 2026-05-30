eab60a943e90dfb5f47392de9c46394c5e7d2426
Mohith <mohithrammallapureddy76@gmail.com>
2026-05-30 22:00:28 +0530

    learning usage

--- samplecode.sh
+++ samplecode.sh
@@ -1,1 +1,13 @@
 "welcome to demo project"
+Activity 1 - to learn simple github actions
+1. login or sign up into github.com
+2. create git repo using '+' in the top bar of the page
+3. provide name 'demo project', select for public button, click initialize readme set to on, create repository
+4. click on add file > create new file > name the file as samplecode.sh, write content - echo "welcome to demo project">click on commit changes button>name the commit - Add welcome message to samplecode.sh, click on 'Create a new branch for this commit and start a pull request', click on propose changes button
+5. you will be moved to page open a pull request, in the page write description 'review changes made to the samplecode.sh and accept'>click on create pull request
+6. now click on merge pull request and next click confirm merge to confirm
+7. open main branch and verify by checking that new changes are merged we can see merged pull request...
+8. now open new branch we created to edit exisiting file, click on samplecode.sh, click on pencil icon to edit the file, make changes like adding or removing anything, eg: i have add few words in the line of code echo "welcome to demo project and get started", click on preview, the green in color are the new changes made in the code, click on commit changes, write commit message, and click commit changes
+9. in the new branch we have made a commit, open the new branch we can see the message 'This branch is 1 commit ahead of and 1 commit behind main.'
+10. click on the message>create new pull request> write description like 'updated the message, review the changes and accept' and create pull request> click on merge pull request> confirm merge
+11. open main branch, open file to see the changes
